a91a937318dbeeb5b68853bd7685aa64b0517d6c
Katalin Voss <ge52lix@mytum.de>
2022-10-27 20:05:12 -0700

    adjust tsv_to_fasta.py to make more readable and also catch edge case when sequence is not in sequence column but in junction column

--- Download_data/tsv_to_fasta.py
+++ Download_data/tsv_to_fasta.py
@@ -2,6 +2,7 @@
 ## extract only the sequence ID and the sequence column of interest
 ## for the input file
 import argparse
+import pandas as pd
 
 parser = argparse.ArgumentParser()
 # Add an argument
@@ -9,19 +10,21 @@ parser.add_argument('-i', '--input_tsv', type=str, required=True)
 parser.add_argument('-o', '--output', type=str, required=True)
 # Parse the argument
 args = parser.parse_args()
-# Print "Hello" + the user input argument
+
 x = open(args.input_tsv, "r")
 z = x.readlines()
-y = open(args.output, "w")
+f = open(args.output, "w")
+columns = z[0].split("\t")
+id = columns.index("sequence_id")
+sequence = columns.index("sequence")
+junction = columns.index("junction")
+
 l = len(z)
-#TODO: adapt so it can read files like minervina
-for i in range(2, l, 1):
-    f = z[i]
-    l1 = len(f)
-    if "\t" in f:
-        n = f.find("\t")
-        #id = f[0:(n)]
-        s = f[(n + 1):l1].find("\t")
-        #seq = f[(n + 1):(s+1)]
-        y.write(">" + f[0:(n)] + "\n")
-        y.write(f[(n + 1):(s+1)] + "\n")
+for i in range(1, l, 1):
+    line = z[i].split("\t")
+    id_i = line[id]
+    seq = line[sequence]
+    if seq == "" and line[junction] != "":
+        seq = line[junction]
+    f.write(">" + id_i + "\n" + seq + "\n")
+f.close()
