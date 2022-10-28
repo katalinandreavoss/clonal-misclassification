0afc21865afefab26bc742a3c3a151961b281ed9
Katalin Voss <ge52lix@mytum.de>
2022-10-27 21:21:29 -0700

    remove --locus flag

--- partis/partis_simulation/simulate.sh
+++ partis/partis_simulation/simulate.sh
@@ -16,7 +16,7 @@ do
     esac
 done
 
-$partis cache-parameters --infname $fasta  --parameter-dir $output --locus
+$partis cache-parameters --infname $fasta  --parameter-dir $output
 
 # partition the fasta file used in cache-parameters to create multi-hmm folder in the parameter directory
 #bin/partis partition --infname name.fasta --outfname name_pd.yaml --parameter-dir folder_name --count-parameters
