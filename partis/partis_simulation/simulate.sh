cff230ac00e6ab48926dc459f9d1553f37e874db
Katalin Voss <ge52lix@mytum.de>
2022-10-27 21:10:25 -0700

    first draft partis simulation

---
+++ partis/partis_simulation/simulate.sh
@@ -0,0 +1,51 @@
+##### Code to simulate sequences #####
+## simulate sequences in partis
+
+
+########## Partis Original ##########
+
+##### Step 1: Parameter directory ####
+
+## creating parameter directory from a fasta file 
+while getopts p:f:o: flag
+do
+    case "${flag}" in
+        p) partis=${OPTARG};;
+        f) fasta=${OPTARG};;
+        o) output=${OPTARG}
+    esac
+done
+
+$partis cache-parameters --infname $fasta  --parameter-dir $output
+
+# partition the fasta file used in cache-parameters to create multi-hmm folder in the parameter directory
+#bin/partis partition --infname name.fasta --outfname name_pd.yaml --parameter-dir folder_name --count-parameters
+
+##### Step 2: Simulate Sequences #####
+
+## simulate and rearrange from scratch:
+
+#set x for number of desired simulations
+#for i in $(seq 1 x); do echo “$i”; bin/partis simulate --parameter-dir folder_name --n-sim-events integer --outfname $i.yaml; done
+ 
+
+#########################################################################################################################
+
+########## Partis Second Version ##########
+# run the simulation on Docker
+# code files are in a folder called python_test
+# change the input for n-sim-events, n-leaves, and shm-freq
+ 
+# autosimulator function
+#python python_test/autosimulator2.py --species human --n-procs 1 --yaml-folder run_1_yaml --n-sim-events 5 --n-leaves 5 --fasta-folder run_1_fasta --shm-freq 0.05 --generate-germline 
+
+# move the yaml files from the sub folders in run_1_yaml into a separate folder called run_yaml (there should be no subfolders)
+
+# partition function
+# change the n-max-final-cluster and n-partitions-to-write (default is 10 but it may write less)
+#python python_test/auto_partition.py --n-procs 1 --n-max-final-cluster <input value> --n-partitions-to-write  <input value> --input-folder run_yaml --output-folder run_parition  --is-simu
+ 
+# partition summarizer function
+#python python_test/partition_summarizer.py --input-folder run_parition --outfname run_summarizer --is-simu
+
+
