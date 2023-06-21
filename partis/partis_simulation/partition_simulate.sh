f752a8f696ad2adc002aa947e10a56a98ea706f6
Katalin Voss <ge52lix@mytum.de>
2023-06-21 13:58:27 -0700

    add partition part and simulation part, add partition_simulate rule to Snakefile

---
+++ partis/partis_simulation/partition_simulate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+while getopts p:f:o: flag
+do
+    case "${flag}" in
+        p) partis=${OPTARG};;
+        f) fasta=${OPTARG};;
+        o) output=${OPTARG};;
+    esac
+done
+
+# partition the fasta file used in cache-parameters to create multi-hmm folder in the parameter directory
+$partis partition --infname $fasta --outfname $output/pd.yaml --parameter-dir $output --count-parameters
+
+##### Step 2: Simulate Sequences #####
+
+## simulate and rearrange from scratch:
+
+#set x for number of desired simulations $(seq 1 x)
+for i in $(seq 1 5); do echo "$i"; $partis simulate --parameter-dir $output --n-sim-events integer --outfname $i.yaml; done
