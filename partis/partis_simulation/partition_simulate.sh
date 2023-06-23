e9b90b847d3cbdfa548b9ecfe668a77f22e1795e
Katalin Voss <ge52lix@mytum.de>
2023-06-22 21:00:59 -0700

    partition only

--- partis/partis_simulation/partition_simulate.sh
+++ partis/partis_simulation/partition_simulate.sh
@@ -18,7 +18,7 @@ $partis partition --infname $fasta --outfname $output/pd.yaml --parameter-dir $o
 ## simulate and rearrange from scratch:
 
 #set x for number of desired simulations $(seq 1 x)
-for i in $(seq 1 5); do
-  echo "$i";
-  $partis simulate --parameter-dir $output --n-sim-events ${i} --outfname sim_${i}.yaml --min-observations-per-gene 5 --n-leaf-distribution geometric;
-done
+#for i in $(seq 1 5); do
+#  echo "$i";
+#  $partis simulate --parameter-dir $output --n-sim-events ${i} --outfname sim_${i}.yaml --min-observations-per-gene 5 --n-leaf-distribution geometric;
+#done
