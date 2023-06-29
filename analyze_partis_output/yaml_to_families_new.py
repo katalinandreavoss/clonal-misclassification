957a48935314c24d02e8e14775d0c49cba1ee200
Katalin Voss <ge52lix@mytum.de>
2023-06-29 09:46:52 -0700

    use partis python script to extract families

---
+++ analyze_partis_output/yaml_to_families_new.py
@@ -0,0 +1,39 @@
+import sys
+import subprocess
+import yaml
+from pathlib import Path
+
+yaml_file = sys.argv[1]  # .yaml file from partis partition as input
+partis_dir = sys.argv[2]
+output_dir = sys.argv[3]
+
+#c1 = open(sys.argv[2], "w")  # .txt file for partitions with sequences in one file
+#c2 = open(sys.argv[3], "w")
+
+conf = yaml.safe_load(Path(yaml_file).read_text())
+
+n=0
+for key,val in conf.items():
+    if key == "partitions":
+        n_partitions = len(key)
+        for i in range(n_partitions):
+            result = subprocess.run(["python", partis_dir+"/bin/parse-output.py", yaml_file, output_dir+"/partition_"+str(i)+".fasta"  "--partition-index", i], capture_output=True, text=True, check=False)
+            print(result.stdout)
+            #print("partition"+str(i))
+
+        #for e in val:
+        #    n = n+1
+        #    c1.write("partition" + "_" + str(n) + "\n")
+        #    c2.write(">partition" + "_" + str(n) + "\n")
+        #    for seq, v in e.items():
+        #        if seq == "input_seqs":
+        #            l1 = len(v)
+        #            for k in range(0, l1, 1):
+        #                c1.write("fam" + str(n) + "seq" + str(k + 1) + "\n" + v[k] + "\n")
+        #        if seq == "naive_seq":
+        #            c2.write(v+"\n")
+
+#c1.flush()
+#c2.flush()
+#c1.close()
+#c2.close()
