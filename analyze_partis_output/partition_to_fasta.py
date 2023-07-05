36c3028f02c243cf87ca38dc9da892e930515ae7
Katalin Voss <ge52lix@mytum.de>
2023-07-04 22:16:25 -0700

    merge

--- analyze_partis_output/partition_to_fasta.py
+++ analyze_partis_output/partition_to_fasta.py
@@ -1,10 +1,11 @@
 import sys
 a=open(sys.argv[1],"r") #input is .txt file containing sequences of all clonal families (output of yaml_to_families.py)
 b= a.readlines()
+output=sys.argv[2]
 l= len(b)
 for i in range(0,l,1):
 	if "partition" in b[i]:
-		x= str(b[i]) + ".fasta" 
+		x= output+str(b[i]) + ".fasta"
 		c=open(x,"w") #creating .fasta file for each partition
 	else:
 		if "seq" in b[i]:
