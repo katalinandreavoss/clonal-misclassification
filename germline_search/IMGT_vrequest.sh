7c9327b094a1ebb491faf72fdc3f0ff6991aa03c
Katalin Voss <ge52lix@mytum.de>
2023-07-07 14:22:18 -0700

    cd

--- germline_search/IMGT_vrequest.sh
+++ germline_search/IMGT_vrequest.sh
@@ -13,5 +13,6 @@ done
 for fasta in $directory/*.fasta; do
   name=${fasta%.fasta}
   name=${name##*/}
+  cd ${output}
   python -m vquest --species human --receptorOrLocusType IG --fileSequences ${fasta} -o ${output} && mv ${output}/001 ${output}/${name}
 done
