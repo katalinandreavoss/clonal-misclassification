b1f0f81cdf1f89b766b5e0915e4b9b5e83508b4b
katalinandreavoss <kavoss@endeavour2.hpc.usc.edu>
2023-07-28 16:22:14 -0700

    finish plot for comparing family sizes and number of families

--- simulation_analyses/plot_number_families.R
+++ simulation_analyses/plot_number_families.R
@@ -1,11 +1,25 @@
+#!/usr/bin/env Rscript
+#library("optparse")
 library(data.table)
 library(tidyverse)
 library(patchwork)
 
+#option_list = list(
+#  make_option(c("-p", "--path"), type="character", default=NULL, 
+#              help="path to simulations", metavar="character"),
+#  make_option(c("-o", "--out"), type="character", default="out.txt", 
+#              help="output file name [default= %default]", metavar="character")
+#); 
+
+#opt_parser = OptionParser(option_list=option_list);
+#opt = parse_args(opt_parser);
+
 
 clonal_families<-c(2,4,6,8)
-
+#opt$path
+#path<-paste(opt$path,"sim_",sep = "")
 path<-"/home1/kavoss/simulation_from_scratch/sim_"
+
 
 get_max <- function(file_path) {
   df<-fread(file_path)
@@ -21,6 +35,7 @@ get_freq<-function(file_path) {
   frequencies<-as.data.frame(table(df$V1))
   return(frequencies$Freq)
 }
+
 
 for (x in clonal_families) {
   print(x)
@@ -38,6 +53,16 @@ for (x in clonal_families) {
   
   freq_table<-total_df[,c("SHM","freq")]
   freq_table<-freq_table %>% unnest(freq)
+
+  real_freq <-
+    filenames <- list.files(path=curr_path, pattern="family_sizes.txt", full.names=TRUE,recursive = TRUE)%>% 
+    map_df(~fread(.))
+  real_freq$V1<-gsub("_",".",real_freq$V1)
+  colnames(real_freq)<-c("SHM","freq")
+  freq_total<-rbind(freq_table,real_freq)
+  freq_total<-rbindlist(list(freq_table, real_freq), idcol = 'data')[, data:= paste0('set', data)][]
+  freq_total[data=="set1"]$data<-"discerned families"
+  freq_total[data=="set2"]$data<-"real families"
   
   title<-paste(as.character(x), "original families")
   p<-ggplot(total_df, aes(SHM,number_families)) + 
@@ -46,9 +71,9 @@ for (x in clonal_families) {
     ylim(0, 450)+
     ggtitle(title)
 
-  m<-ggplot(freq_table, aes(SHM,freq)) + 
+  m<-ggplot(freq_total, aes(SHM,freq, fill=data)) + 
     geom_boxplot()+
-    ylim(0, 200)+
+    ylim(0, 300)+
     ggtitle(title)
   
   assign(paste("p",as.character(x),sep = ""),p)
@@ -56,10 +81,11 @@ for (x in clonal_families) {
 }
 
 
-(p2+p4)/(p6+p8)+ plot_annotation('number of discerned families (PTP)',theme=theme(plot.title=element_text(hjust=0.5)))
+number<-(p2+p4)/(p6+p8)+ plot_annotation('number of discerned families (PTP)',theme=theme(plot.title=element_text(hjust=0.5)))
 
-(m2+m4)/(m6+m8)+ plot_annotation('size of discerned families (PTP)',theme=theme(plot.title=element_text(hjust=0.5)))
-
-
+size<-(m2+m4)/(m6+m8)+ plot_annotation('size of discerned families (PTP)',theme=theme(plot.title=element_text(hjust=0.5)))
+path<-"/home1/kavoss/simulation_from_scratch/"
+ggsave(paste(path,'numer_disc_families.png',sep = ""), number)
+ggsave(paste(path,'size_disc_families.png',sep = ""), size)
 
 
