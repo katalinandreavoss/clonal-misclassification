e20161865dba4eabd2f41fa5c1e63e6204c31dae
katalinandreavoss <kavoss@endeavour2.hpc.usc.edu>
2023-07-31 17:24:54 -0700

    change simulate script so its easier to adapt for different parameters

--- partis/partis_simulation/simulate.sh
+++ partis/partis_simulation/simulate.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
-while getopts p:o: flag
+while getopts p:o:c:s:l:i: flag
 do
     # shellcheck disable=SC2220
     case "${flag}" in
         p) partis=${OPTARG};;
         o) output=${OPTARG};;
+        c) clones=${OPTARG};;
+        s) shm=${OPTARG};;
+        l) leaves=${OPTARG};;
+        i) sim=${OPTARG};;
     esac
 done
 
 ##### Step 2: Simulate Sequences #####
 #test
 ## simulate and rearrange from scratch:
-clonal_families=${output##*sim_}
-clonal_families=${clonal_families%/*}
-shm=${output##*/}
-echo ${shm}
-echo ${clonal_families}
 
-$partis simulate --simulate-from-scratch --n-sim-events ${clonal_families} --scratch-mute-freq ${shm//_/.} --n-leaves 50 --outfname $output/sim_${shm}.yaml --debug 1
+
+$partis simulate --simulate-from-scratch --n-sim-events ${clones} --scratch-mute-freq ${shm//_/.} --n-leaves ${leaves} --outfname $output/clones_${clones}_shm_${shm}_leaves_${leaves}_sim_${sim}.yaml --debug 1
+
+
+#clonal_families=${output##*sim_}
+#clonal_families=${clonal_families%/*}
+#shm=${output##*/}
+#echo ${shm}
+#echo ${clonal_families}
 
 #set x for number of desired simulations $(seq 1 x)
 #for i in 0.01 0.05 0.1 0.2 0.3; do
