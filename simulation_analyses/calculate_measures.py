c72451594c4520b7fc7d5bd9b5690e6962a4ee2d
katalinandreavoss <kavoss@endeavour2.hpc.usc.edu>
2023-09-28 18:34:46 -0700

    cleanup script for combining results, add plotting script for big simulation

--- simulation_analyses/calculate_measures.py
+++ simulation_analyses/calculate_measures.py
@@ -1,14 +1,126 @@
 import sys
-import yaml
 import pandas as pd
+import statistics
+path = sys.argv[1]
 
-#path = sys.argv[1] 
+#path = "/Users/kavoss/Documents/Research/simulations/14/0_2/10/6/"
+params = path.split("/")
+sim = params[-2]
+balance = params[-3]
+leaves = params[-4]
+SHM = params[-5]
+clones = params[-6]
+complete = open(path+"analysis_complete.tsv", 'w')
+without_singletons = open(path+"analysis_no_singletons.tsv", 'w')
+
+complete.write("tool\tclones\tSHM\tleaves\tbalance\tnum_families\tmedian_size_families\tvar_size_families\tsingletons\treal_num_fam\treal_med_size\treal_var_size\n")
+without_singletons.write("tool\tclones\tSHM\tleaves\tbalance\tnum_families\tmedian_size_families\tvar_size_families\tsingletons\treal_num_fam\treal_med_size\treal_var_size\n")
+
+mixcr_path = path+"clean.fasta.vdjca.clns_IGH.tsv"  # .yaml file from partis partition as input
+changeo_path = path+"vquest_files/combined_db-pass_clone-pass.tsv"
+scoper1_path = path+"results_db_idClones.tsv"
+scoper2_path = path+"results_hierClones.tsv"
+scoper3_path = path+"results_specClones.tsv"
+real_values_path = path+"family_sizes.txt"
+
+real_values = pd.read_csv(real_values_path, header=None)
+real_values_num_fam = len(real_values)
+real_values_med_size = statistics.median(real_values[4])
+real_values_var_size = statistics.variance(real_values[4])
+
+def mixcr_get_med_var(df):
+    if len(df)>1:
+        mixcr_med_size = statistics.median(df)
+        mixcr_var_size = statistics.variance(df)
+    elif len(df)==1:
+        mixcr_med_size = statistics.median(df)
+        mixcr_var_size = "NaN"
+    else:
+        mixcr_med_size = "NaN"
+        mixcr_var_size = "NaN"
+    return mixcr_med_size, mixcr_var_size
+
+mixcr = pd.read_csv(mixcr_path,sep='\t')
+mixcr_num_fam = len(mixcr)
+mixcr_med_size, mixcr_var_size = mixcr_get_med_var(mixcr["readCount"])
+mixcr_singletons = len(mixcr[mixcr["readCount"]==1])
+complete.write("mixcr\t"+clones+"\t"+SHM+"\t"+leaves+"\t"+balance+"\t"+str(mixcr_num_fam)+"\t"+str(mixcr_med_size)+"\t"+str(mixcr_var_size)+"\t"+str(mixcr_singletons)+"\t"+str(real_values_num_fam)+"\t"+str(real_values_med_size)+"\t"+str(real_values_var_size)+"\n")
+
+#without singletons
+mixcr_num_fam = len(mixcr[mixcr["readCount"]!=1])
+mixcr_singletons = len(mixcr[mixcr["readCount"]!=1])
+mixcr_med_size, mixcr_var_size = mixcr_get_med_var(mixcr[mixcr["readCount"]!=1]["readCount"])
+without_singletons.write("mixcr\t"+clones+"\t"+SHM+"\t"+leaves+"\t"+balance+"\t"+str(mixcr_num_fam)+"\t"+str(mixcr_med_size)+"\t"+str(mixcr_var_size)+"\t"+str(mixcr_singletons)+"\t"+str(real_values_num_fam)+"\t"+str(real_values_med_size)+"\t"+str(real_values_var_size)+"\n")
 
 
-path="/panfs/qcb-panasas/kavoss/method_comparison/20/0_2/50/1_0/21"
-mixcr = path+"clean.fasta.vdjca.clns_IGH.tsv"  # .yaml file from partis partition as input
-changeo = path+"vquest_files/combined_db-pass_clone-pass.tsv"
-scoper1 = path+"results_db_idClones.tsv"
-scoper2 = path+"results_db_hierClones.tsv"
-scoper3 = path+"results_db_specClones.tsv"
-real_values = path+"family_sizes.txt"
+def get_med_var(df):
+    if len(df)>1:
+        med_size = statistics.median(df)
+        var_size = statistics.variance(df)
+    elif len(df)==1:
+        med_size = statistics.median(df)
+        var_size = "NaN"
+    else:
+        med_size = "NaN"
+        var_size = "NaN"
+    return med_size,var_size
+
+changeo = pd.read_csv(changeo_path,sep='\t')
+changeo_num_fam = max(changeo["clone_id"])
+changeo_grouped = changeo.groupby(['clone_id'])['sequence_id'].count()
+changeo_med_size,changeo_var_size = get_med_var(changeo_grouped)
+changeo_singletons = changeo.groupby(['clone_id'])['sequence_id'].filter(lambda x: len(x) == 1).count()
+complete.write("changeo\t"+clones+"\t"+SHM+"\t"+leaves+"\t"+balance+"\t"+str(changeo_num_fam)+"\t"+str(changeo_med_size)+"\t"+str(changeo_var_size)+"\t"+str(changeo_singletons)+"\t"+str(real_values_num_fam)+"\t"+str(real_values_med_size)+"\t"+str(real_values_var_size)+"\n")
+
+#without singletons
+changeo_num_fam = changeo.groupby(['clone_id'])['sequence_id'].filter(lambda x: len(x) > 1).count()
+changeo_grouped = changeo_grouped[changeo.groupby(['clone_id'])['sequence_id'].apply(lambda x: len(x) > 1)]
+changeo_med_size,changeo_var_size = get_med_var(changeo_grouped)
+without_singletons.write("changeo\t"+clones+"\t"+SHM+"\t"+leaves+"\t"+balance+"\t"+str(changeo_num_fam)+"\t"+str(changeo_med_size)+"\t"+str(changeo_var_size)+"\t"+str(changeo_singletons)+"\t"+str(real_values_num_fam)+"\t"+str(real_values_med_size)+"\t"+str(real_values_var_size)+"\n")
+
+
+scoper1 = pd.read_csv(scoper1_path,sep='\t')
+scoper1_num_fam = max(scoper1["clone_id"])
+scoper1_grouped = scoper1.groupby(['clone_id'])['sequence_id'].count()
+scoper1_med_size,scoper1_var_size = get_med_var(scoper1_grouped)
+scoper1_singletons = scoper1.groupby(['clone_id'])['sequence_id'].filter(lambda x: len(x) == 1).count()
+complete.write("scoper_id\t"+clones+"\t"+SHM+"\t"+leaves+"\t"+balance+"\t"+str(scoper1_num_fam)+"\t"+str(scoper1_med_size)+"\t"+str(scoper1_var_size)+"\t"+str(scoper1_singletons)+"\t"+str(real_values_num_fam)+"\t"+str(real_values_med_size)+"\t"+str(real_values_var_size)+"\n")
+
+#without singletons
+scoper1_num_fam = scoper1.groupby(['clone_id'])['sequence_id'].filter(lambda x: len(x) > 1).count()
+scoper1_grouped = scoper1_grouped[scoper1.groupby(['clone_id'])['sequence_id'].apply(lambda x: len(x) > 1)]
+scoper1_med_size,scoper1_var_size = get_med_var(scoper1_grouped)
+without_singletons.write("scoper_id\t"+clones+"\t"+SHM+"\t"+leaves+"\t"+balance+"\t"+str(scoper1_num_fam)+"\t"+str(scoper1_med_size)+"\t"+str(scoper1_var_size)+"\t"+str(scoper1_singletons)+"\t"+str(real_values_num_fam)+"\t"+str(real_values_med_size)+"\t"+str(real_values_var_size)+"\n")
+
+
+scoper2 = pd.read_csv(scoper2_path,sep='\t')
+scoper2_num_fam = max(scoper2["clone_id"])
+scoper2_grouped = scoper2.groupby(['clone_id'])['sequence_id'].count()
+scoper2_med_size, scoper2_var_size = get_med_var(scoper2_grouped)
+scoper2_singletons = scoper2.groupby(['clone_id'])['sequence_id'].filter(lambda x: len(x) == 1).count()
+complete.write("scoper_hier\t"+clones+"\t"+SHM+"\t"+leaves+"\t"+balance+"\t"+str(scoper2_num_fam)+"\t"+str(scoper2_med_size)+"\t"+str(scoper2_var_size)+"\t"+str(scoper2_singletons)+"\t"+str(real_values_num_fam)+"\t"+str(real_values_med_size)+"\t"+str(real_values_var_size)+"\n")
+
+#without singletons
+scoper2_num_fam = scoper2.groupby(['clone_id'])['sequence_id'].filter(lambda x: len(x) > 1).count()
+scoper2_grouped = scoper2_grouped[scoper2.groupby(['clone_id'])['sequence_id'].apply(lambda x: len(x) > 1)]
+scoper2_med_size, scoper2_var_size = get_med_var(scoper2_grouped)
+without_singletons.write("scoper_hier\t"+clones+"\t"+SHM+"\t"+leaves+"\t"+balance+"\t"+str(scoper2_num_fam)+"\t"+str(scoper2_med_size)+"\t"+str(scoper2_var_size)+"\t"+str(scoper2_singletons)+"\t"+str(real_values_num_fam)+"\t"+str(real_values_med_size)+"\t"+str(real_values_var_size)+"\n")
+
+
+scoper3 = pd.read_csv(scoper3_path,sep='\t')
+scoper3_num_fam = max(scoper3["clone_id"])
+scoper3_grouped = scoper3.groupby(['clone_id'])['sequence_id'].count()
+scoper3_med_size, scoper3_var_size = get_med_var(scoper3_grouped)
+scoper3_singletons = scoper3.groupby(['clone_id'])['sequence_id'].filter(lambda x: len(x) == 1).count()
+complete.write("scoper_spec\t"+clones+"\t"+SHM+"\t"+leaves+"\t"+balance+"\t"+str(scoper3_num_fam)+"\t"+str(scoper3_med_size)+"\t"+str(scoper3_var_size)+"\t"+str(scoper3_singletons)+"\t"+str(real_values_num_fam)+"\t"+str(real_values_med_size)+"\t"+str(real_values_var_size)+"\n")
+
+#without singletons
+scoper3_num_fam = scoper3.groupby(['clone_id'])['sequence_id'].filter(lambda x: len(x) > 1).count()
+scoper3_grouped = scoper3_grouped[scoper3.groupby(['clone_id'])['sequence_id'].apply(lambda x: len(x) > 1)]
+scoper3_med_size, scoper3_var_size = get_med_var(scoper3_grouped)
+without_singletons.write("scoper_spec\t"+clones+"\t"+SHM+"\t"+leaves+"\t"+balance+"\t"+str(scoper3_num_fam)+"\t"+str(scoper3_med_size)+"\t"+str(scoper3_var_size)+"\t"+str(scoper3_singletons)+"\t"+str(real_values_num_fam)+"\t"+str(real_values_med_size)+"\t"+str(real_values_var_size)+"\n")
+
+complete.flush()
+complete.close()
+without_singletons.flush()
+without_singletons.close()
---
+++ simulation_analyses/plots_big_simulation.R
@@ -0,0 +1,133 @@
+library(data.table)
+library(tidyverse)
+library(patchwork)
+library(Biostrings)
+library(reshape2)
+library(stringr)
+library(patchwork)
+clonal_families<-c(4,6,8,10,12,14,16,18, 20)
+tools<-c("PTP","MiXCR", "changeo","scoper_ID","scoper_hierarchical","scoper_spectral")
+SHM<-c("0_001","0_005", "0_01","0_05","0_1","0_2")
+leaves <- c("10","20","50","100")
+balance <- c("0_0","0_3","0_5","1_0","1_3")
+sims<-seq(1,50,1)
+path<-"/home1/kavoss/panfs/method_comparison/"
+
+get_MSE_median_fam_size<-function(data,df) {
+  clone=data[['clones']]
+  shm=data[['SHM']]
+  leaf=data[['leaves']]
+  balances=data[['balance']]
+  which_tool=data[['tool']]
+  real<-df%>%
+    filter(clones == clone)%>%
+    filter(SHM == shm)%>%
+    filter(leaves == leaf)%>%
+    filter(balance == balances)%>%
+    filter(tool == which_tool)%>%
+    select(real_med_size)
+  pred<-df%>%
+    filter(clones == clone)%>%
+    filter(SHM == shm)%>%
+    filter(leaves == leaf)%>%
+    filter(balance == balances)%>%
+    filter(tool == which_tool)%>%
+    select(median_size_families)
+  mse<-mean((as.numeric(real$real_med_size) - as.numeric(pred$median_size_families))^2)
+  return(mse)
+}
+
+get_MSE_fam_number<-function(data,df) {
+  clone=data[['clones']]
+  shm=data[['SHM']]
+  leaf=data[['leaves']]
+  balances=data[['balance']]
+  which_tool=data[['tool']]
+  real<-df%>%
+    filter(clones == clone)%>%
+    filter(SHM == shm)%>%
+    filter(leaves == leaf)%>%
+    filter(balance == balances)%>%
+    filter(tool == which_tool)%>%
+    select(real_num_fam)
+  pred<-df%>%
+    filter(clones == clone)%>%
+    filter(SHM == shm)%>%
+    filter(leaves == leaf)%>%
+    filter(balance == balances)%>%
+    filter(tool == which_tool)%>%
+    select(num_families)
+  mse<-mean((as.numeric(real$real_num_fam) - as.numeric(pred$num_families))^2)
+  return(mse)
+}
+
+
+
+for (x in clonal_families) {
+  print(x)
+  curr_path<-paste(path,as.character(x),sep = "")
+  filenames <- list.files(path=curr_path, pattern="analysis_complete.tsv", full.names=TRUE,recursive = TRUE)
+  total_df <- do.call(rbind,lapply(filenames,read.csv,sep="\t"))
+  
+  
+  number_families<-tidyr::expand_grid(tools,clonal_families,SHM, leaves,balance)
+  colnames(number_families)<- c("tool","clones","SHM","leaves","balance")
+  
+  
+  number_families$MSE_fam_size<-apply(number_families,1, FUN=get_MSE_median_fam_size, df=total_df)
+  
+  number_families$MSE_num_fam<-apply(number_families,1, FUN=get_MSE_fam_number, df=total_df)
+  
+  assign(paste("clones",as.character(x),sep = ""),number_families)
+  assign(paste("all",as.character(x),sep = ""),total_df)
+}
+
+
+combined<-rbindlist(list(clones4,clones6, clones8,clones10,clones12,clones14,clones16,clones18,clones20))
+
+combined$clones<-as.character(combined$clones)
+
+write.csv(combined, "/home1/kavoss/panfs/simulations/output_w_singletons.csv", row.names=FALSE)
+
+
+MSE_SHM_num_fam<-ggplot(combined, aes(SHM,MSE_num_fam, fill=tool)) + 
+  geom_boxplot()+
+  scale_y_continuous(trans='log10')+
+  ylab("Mean Squared Error - Number of Families (log)")
+
+MSE_clones_num_fam<-ggplot(combined, aes(clones,MSE_num_fam, fill=tool)) + 
+  geom_boxplot()+
+  scale_x_discrete(limits = unique(combined$clones))+
+  scale_y_continuous(trans='log10')+
+  ylab("Mean Squared Error - Number of Families (log)")
+
+
+
+MSE_leaves_num_fam<-ggplot(combined, aes(leaves,MSE_num_fam, fill=tool)) + 
+  geom_boxplot()+
+  scale_x_discrete(limits = unique(combined$leaves))+
+  scale_y_continuous(trans='log10')+
+  ylab("Mean Squared Error - Number of Families (log)")
+
+
+MSE_SHM_size_fam<-ggplot(combined, aes(SHM,MSE_fam_size, fill=tool)) + 
+  geom_boxplot()+
+  scale_y_continuous(trans='log10')+
+  ylab("Mean Squared Error - Median Family Size (log)")
+
+MSE_clones_size_fam<-ggplot(combined, aes(clones,MSE_fam_size, fill=tool)) + 
+  geom_boxplot()+
+  scale_x_discrete(limits = unique(combined$clones))+
+  scale_y_continuous(trans='log10')+
+  ylab("Mean Squared Error - Median Family Size (log)")
+
+MSE_leaves_size_fam<-ggplot(combined, aes(leaves,MSE_fam_size, fill=tool)) + 
+  geom_boxplot()+
+  scale_x_discrete(limits = unique(combined$leaves))+
+  scale_y_continuous(trans='log10')+
+  ylab("Mean Squared Error - Median Family Size (log)")
+
+
+MSE_SHM_num_fam+MSE_clones_num_fam+ MSE_leaves_num_fam+plot_annotation('number of discerned families',theme=theme(plot.title=element_text(hjust=0.5)))+ plot_layout(guides = "collect")
+MSE_SHM_size_fam+MSE_clones_size_fam+ MSE_leaves_size_fam+plot_annotation('median size of discerned families',theme=theme(plot.title=element_text(hjust=0.5)))+ plot_layout(guides = "collect")
+
